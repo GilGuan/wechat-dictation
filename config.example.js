c31411ad89bdd8e582ee154f9f3153fe69b1b9cf
Gil <hua_guan@kingdee.com>
2026-03-27 18:51:00 +0800

    将config.js添加到.gitignore，添加配置示例文件

---
+++ config.example.js
@@ -0,0 +1,6 @@
+// 配置文件示例
+// 复制此文件为 config.js 并填入你的云开发环境ID
+module.exports = {
+  // 云开发环境ID
+  cloudEnvId: 'your-cloud-env-id'
+}
--- config.js
+++
@@ -1,5 +0,0 @@
-// 配置文件
-module.exports = {
-  // 云开发环境ID
-  cloudEnvId: 'gilguan-5gmuwxiu84ee098b'
-}
